704642fa92a80a51ed6f88a6283b3bcf006bfaa7
박범준 <blackss2@nate.com>
2015-04-10 23:15:05 +0900

    Test finish

--- api/caller.go
+++ api/caller.go
@@ -139,7 +139,6 @@ func (this *Caller) Call() (code int, ret interface{}) {
 	buffer.WriteString(this.engine.host)
 	urlPath := this.url
 	if len(this.Params.Params) > 0 {
-		fmt.Println(urlPath)
 		urlList := strings.Split(urlPath, "/")
 		urlHash := make(map[string]int)
 		for i, v := range urlList {
@@ -151,7 +150,6 @@ func (this *Caller) Call() (code int, ret interface{}) {
 			}
 		}
 		urlPath = strings.Join(urlList, "/")
-		fmt.Println(urlPath)
 	}
 	buffer.WriteString(urlPath)
 	if len(this.QParams) > 0 {
