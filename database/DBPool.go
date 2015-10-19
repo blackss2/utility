2c3b88881c56190ebba14dfa52e5869612370e01
blackss2 <blackss2@nate.com>
2015-10-19 18:18:11 +0900

    Revert "rollback"
    This reverts commit e56de36f5e0b9cc045e212d150d1c5c2adf7e375.

--- database/DBPool.go
+++ database/DBPool.go
@@ -32,7 +32,7 @@ func CreateDBPool(driver string, ip string, port int, name string, id string, pw
 }
 
 func CreateDBPoolByConnString(driver string, connString string, poolSize int) *DBPool {
-	pool := &DBPool{driver, connString, poolSize, make(chan *Database, poolSize)}
+	pool := &DBPool{driver, connString, poolSize, make(chan *Database, poolSize), make([]string, 0)}
 	err := pool.fill()
 	if err != nil {
 		fmt.Println(err)
