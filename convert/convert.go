0f04665da9e2112f6db3b81c37bbc65fe23416d5
BumJoon Park <indev@dbusker.com>
2016-04-13 20:37:34 +0900

    change timezone if not described

--- convert/convert.go
+++ convert/convert.go
@@ -146,22 +146,31 @@ func Time(val interface{}) *time.Time {
 			} else if t, err := time.Parse(time.RFC3339, v); err == nil && t.Year() > 0 {
 				return &t
 			} else if t, err := time.Parse("2006-01-02 15:04:05.000", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("2006-01-02 15:04:05", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("2006-01-02", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("20060102150405", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("20060102", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("01-02-06", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("01-02-2006", v); err == nil && t.Year() > 0 {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("15:04:05.000", v); err == nil {
+				t = t.In(time.Local)
 				return &t
 			} else if t, err := time.Parse("15:04:05", v); err == nil {
+				t = t.In(time.Local)
 				return &t
 			}
 		}
