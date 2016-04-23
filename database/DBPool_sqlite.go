06ae048c9b922d2cc5e137bb00cac60beafd0f24
BumJoon Park <indev@dbusker.com>
2016-04-23 10:21:29 +0900

    add sqlite

---
+++ database/DBPool_sqlite.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"reflect"
+	"strings"
+)
+
+func init() {
+	cm := &ConfigMakerBase{
+		keyHash:     make(map[string]string),
+		typeHash:    make(map[string]reflect.Kind),
+		defaultHash: make(map[string]interface{}),
+		templateString: strings.Replace(strings.Replace(`
+			{{ .Path }}
+		`, "\n", "", -1), "\t", "", -1),
+	}
+	cm.AddKey(reflect.String, "Path")
+	AddDriver("sqlite", cm)
+}
